7a83aa7e8fa6bdae6d6f86babc774da55e186119
Shubham Pandey <shubhampandey1964@gmail.com>
2022-09-05 09:15:25 +0530

    Added Middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+
+const jwtSecret = process.env.JWT_SECRET || config.privateKEY
+
+module.exports = (req, res, next) => {
+  /**
+   * @todo could use Authentication: Bearer <token> schema
+   */
+  const token = req.get('x-auth-token');
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({err: {name: err.name, msg: err.message} });
+    } else {
+      req.user = decoded.user;
+      next();
+    }
+  });
+};
